b15257363295aa286c227d047ed65820a8cfe478
Owen Chang <owen.chang@fasprosystems.com>
2019-12-20 15:56:26 +0800

    Add logger

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const winston = require('winston')
+
 const config = {
+	mode: 'dev',
 	ftp: {
 		host: 'ftp.fasmedo.com',
 		user: 'fasmedo',
@@ -11,6 +14,12 @@ const config = {
 	nedb: {
 		filename: './db/test',
     autoload: true
+	},
+	log: {
+		transports: [
+			new winston.transports.Console(),
+			new winston.transports.File({ filename: './log/combined.log' })
+		]
 	}
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const ftp = require('./ftp')
+// const ftp = require('./ftp')
+// const ftp_worker = require('./ftp_worker')
+const Datastore = require('nedb')
+const config = require('./config')
+const jsftp = require('./jsftp')
+const winston = require('winston')
 
-ftp.init()
-ftp.listAll('./', [])
+const debug = require('debug')('files_controller')
+
+debug('test begin')
+
+function FileHandler(cfg = config) {
+	this.ftpcfg = cfg.ftp
+	this.nedbcfg = cfg.nedb
+	this.db = {}
+
+	this.FTP = new jsftp( this.ftpcfg )
+	this.db.cameraFiles = new Datastore( this.nedbcfg )
+	this.logger = winston.createLogger(config.log)
+}
+
+const ff = new FileHandler()
+
+ff.logger.info('Hello distributed log files!')

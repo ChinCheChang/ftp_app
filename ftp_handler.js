2b9872ef52fbab0e6af821690a233fad2457e35d
Owen Chang <owen.chang@fasprosystems.com>
2019-12-23 18:43:17 +0800

    Add logger

--- ftp_handler.js
+++ ftp_handler.js
@@ -1,31 +1,50 @@
-// const ftp = require('./ftp')
-// const ftp_worker = require('./ftp_worker')
 const Datastore = require('nedb')
 const config = require('./config')
-const jsftp = require('./jsftp')
 const winston = require('winston')
+const custJsftp = require('./costomized_jsftp/cust_jsftp')
+const Seq_worker = require('./seq_worker')
+const jsftpPromise = require('./costomized_jsftp/jsftp_promise1')
 
- function FtpFileHandler(cfg = {}) {
-	this.createJsftp(cfg.ftp || config.ftp)
-	this.createNedb('cameraFiles', cfg.nedb || config.nedb)
+function FtpFileHandler(cfg = {}) {
 	this.createLogger(cfg.log || config.log)
-}
-
-FtpFileHandler.prototype.start = function() {
-
-}
-
-FtpFileHandler.prototype.createJsftp = function(cfg) {
-	this.FTP = new jsftp(cfg)
-}
-
-FtpFileHandler.prototype.createNedb = function(dbName, cfg) {
-	this.db = {}
-	this.db[dbName] = new Datastore(cfg)
+	this.createJsftp(cfg.ftp || config.ftp)
+	this.createNedb('cameraFiles', cfg.nedb || config.nedb)	
+	this.createWorker()
 }
 
 FtpFileHandler.prototype.createLogger = function(cfg) {
 	this.logger = winston.createLogger(cfg)
+}
+
+FtpFileHandler.prototype.createJsftp = function(cfg) {
+	this.logger.info('init FTP')
+	this.FTP = new jsftpPromise(custJsftp(cfg), this.logger)
+}
+
+FtpFileHandler.prototype.createNedb = function(dbName, cfg) {
+	this.logger.info('init NeDB')
+	this.db = {}
+	this.db[dbName] = new Datastore(cfg)
+}
+
+FtpFileHandler.prototype.createWorker = function() {
+	this.logger.info('init Worker')
+	this.Worker =  new Seq_worker( this.FTP, (taskInQueue, task) => {
+		return taskInQueue.name === task.name && taskInQueue.parameters[0] == task.parameters[0]
+	}, this.logger)	
+}
+
+FtpFileHandler.prototype.listAll = function(path, array) {
+	return new Promise((resolve, reject) => {			
+		this.Worker.push({
+			name: "listAll",
+			parameters: [
+				path, 
+				() => {	reject('error')	}, 
+				(res) => { resolve(res); }],
+			description: `list ${path}`
+		});							
+	})
 }
 
 module.exports = FtpFileHandler
